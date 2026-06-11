28a568ab4f10ba9d7d2bdc5783d843ef6bffef00
Kenn Herman <kenn@mineshaftgap.net>
2026-06-11 11:40:08 -0700

    quirk_ids: add SE_POLL_SUMMATION and SIREN_BASIC (required by zha)

--- zhaquirks/quirk_ids.py
+++ zhaquirks/quirk_ids.py
@@ -6,6 +6,12 @@ DANFOSS_ALLY_THERMOSTAT = "danfoss.ally_thermostat"
 
 KONKE_BUTTON = "konke.button_remote"  # remote with custom handling in cluster handler
 
+# SE (Smart Energy)
+SE_POLL_SUMMATION = "se.poll_summation"  # SE devices with poll summation cluster
+
+# Siren
+SIREN_BASIC = "siren.basic"  # basic siren with on/off
+
 # Tuya
 TUYA_PLUG_ONOFF = "tuya.plug_on_off_attributes"  # plugs with configurable attributes on the OnOff cluster
 TUYA_PLUG_MANUFACTURER = "tuya.plug_manufacturer_attributes"  # plugs with configurable attributes on a custom cluster
